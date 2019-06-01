aab879593c31d725c6d755d350d6ccdfe403583f
Suzi Rubino <suzi@suzirubi.com>
2019-05-31 22:55:07 -0700

    Create files and coding for flashcards with definitions that click on and off of javascript and jquery terms

---
+++ js/scripts.js
@@ -0,0 +1,179 @@
+$(document).ready (function(){
+  $("#head1").click (function(){
+    $("#definition1").toggle();
+    $("#head1").toggle();
+  });
+  $("#definition1").click (function(){
+    $("#definition1").toggle();
+    $("#head1").toggle();
+  });
+
+  $("#head2").click (function(){
+    $("#definition2").toggle();
+    $(".definition2a").toggle();
+    $("#head2").toggle();
+  });
+  $("#definition2").click (function(){
+    $("#definition2").toggle();
+    $(".definition2a").toggle();
+    $("#head2").toggle();
+  });
+  $(".definition2a").click (function(){
+    $("#definition2").toggle();
+    $(".definition2a").toggle();
+    $("#head2").toggle();
+  });
+
+  $("#head3").click (function(){
+    $("#definition3").toggle();
+    $("#head3").toggle();
+  });
+  $("#definition3").click (function(){
+    $("#definition3").toggle();
+    $("#head3").toggle();
+  });
+
+  $("#head4").click (function(){
+    $("#definition4").toggle();
+    $("#head4").toggle();
+  });
+  $("#definition4").click (function(){
+    $("#definition4").toggle();
+    $("#head4").toggle();
+  });
+
+  $("#head5").click (function(){
+    $("#definition5").toggle();
+    $("#head5").toggle();
+  });
+  $("#definition5").click (function(){
+    $("#definition5").toggle();
+    $("#head5").toggle();
+  });
+
+  $("#head6").click (function(){
+    $("#definition6").toggle();
+    $("#head6").toggle();
+  });
+  $("#definition6").click (function(){
+    $("#definition6").toggle();
+    $("#head6").toggle();
+  });
+
+  $("#head7").click (function(){
+    $("#definition7").toggle();
+    $("#head7").toggle();
+  });
+  $("#definition7").click (function(){
+    $("#definition7").toggle();
+    $("#head7").toggle();
+  });
+
+  $("#head8").click (function(){
+    $("#definition8").toggle();
+    $("#head8").toggle();
+  });
+  $("#definition8").click (function(){
+    $("#definition8").toggle();
+    $("#head8").toggle();
+  });
+
+  $("#head9").click (function(){
+    $("#definition9").toggle();
+    $("#head9").toggle();
+  });
+  $("#definition9").click (function(){
+    $("#definition9").toggle();
+    $("#head9").toggle();
+  });
+
+  $("#head10").click (function(){
+    $("#definition10").toggle();
+    $("#head10").toggle();
+  });
+  $("#definition10").click (function(){
+    $("#definition10").toggle();
+    $("#head10").toggle();
+  });
+
+  $("#head11").click (function(){
+    $("#definition11").toggle();
+    $("#head11").toggle();
+  });
+  $("#definition11").click (function(){
+    $("#definition11").toggle();
+    $("#head11").toggle();
+  });
+
+  $("#head12").click (function(){
+    $("#definition12").toggle();
+    $("#head12").toggle();
+  });
+  $("#definition12").click (function(){
+    $("#definition12").toggle();
+    $("#head12").toggle();
+  });
+
+  $("#head13").click (function(){
+    $("#definition13").toggle();
+    $("#head13").toggle();
+  });
+  $("#definition13").click (function(){
+    $("#definition13").toggle();
+    $("#head13").toggle();
+  });
+
+  $("#head14").click (function(){
+    $("#definition14").toggle();
+    $("#head14").toggle();
+  });
+  $("#definition14").click (function(){
+    $("#definition14").toggle();
+    $("#head14").toggle();
+  });
+
+  $("#head15").click (function(){
+    $("#definition15").toggle();
+    $("#head15").toggle();
+  });
+  $("#definition15").click (function(){
+    $("#definition15").toggle();
+    $("#head15").toggle();
+  });
+
+  $("#head16").click (function(){
+    $("#definition16").toggle();
+    $("#head16").toggle();
+  });
+  $("#definition16").click (function(){
+    $("#definition16").toggle();
+    $("#head16").toggle();
+  });
+
+  $("#head17").click (function(){
+    $("#definition17").toggle();
+    $("#head17").toggle();
+  });
+  $("#definition17").click (function(){
+    $("#definition17").toggle();
+    $("#head17").toggle();
+  });
+
+  $("#head18").click (function(){
+    $("#definition18").toggle();
+    $("#head18").toggle();
+  });
+  $("#definition18").click (function(){
+    $("#definition18").toggle();
+    $("#head18").toggle();
+  });
+
+  $("#head19").click (function(){
+    $("#definition19").toggle();
+    $("#head19").toggle();
+  });
+  $("#definition19").click (function(){
+    $("#definition19").toggle();
+    $("#head19").toggle();
+  });
+});
